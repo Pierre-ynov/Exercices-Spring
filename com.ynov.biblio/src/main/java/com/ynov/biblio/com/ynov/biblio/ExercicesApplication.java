3d4a7ab8343b44ea8f31a28e9322f1ceccf706bb
Pierre VILLIERS <pierre.villiers@ynov.com>
2020-11-06 12:10:50 +0100

    Ajout du fichier de config pour la connexion à la bdd

--- com.ynov.biblio/src/main/java/com/ynov/biblio/com/ynov/biblio/ExercicesApplication.java
+++ com.ynov.biblio/src/main/java/com/ynov/biblio/com/ynov/biblio/ExercicesApplication.java
@@ -2,11 +2,19 @@ package com.ynov.biblio.com.ynov.biblio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ynov.biblio.com.ynov.biblio.config.AppConfiguration;
+
+import lombok.extern.log4j.Log4j;
 
 @SpringBootApplication
 public class ExercicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExercicesApplication.class, args);
+		//SpringApplication.run(ExercicesApplication.class, args);
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfiguration.class);
+		
 	}
 }
---
+++ com.ynov.biblio/src/main/java/com/ynov/biblio/com/ynov/biblio/config/AppConfiguration.java
@@ -0,0 +1,22 @@
+package com.ynov.biblio.com.ynov.biblio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class AppConfiguration {
+
+	public DriverManagerDataSource providerSource() {
+		DriverManagerDataSource ds=  new DriverManagerDataSource();
+		ds.setUsername("root");
+        ds.setPassword("");
+        ds.setUrl("jdbc:mysql://localhost:3306/feedback?useSSL=false&serverTimezone=UTC");
+        return ds;
+	}
+	
+	
+	public JdbcTemplate applicationDataConnection() {
+		return new JdbcTemplate(providerSource());
+	}
+}
