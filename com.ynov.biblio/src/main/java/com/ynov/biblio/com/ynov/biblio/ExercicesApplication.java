3122bd5ab4fe866b5a095cee6d3c786bca7aab73
Pierre VILLIERS <pierre.villiers@ynov.com>
2020-11-06 10:16:07 +0100

    projet maven vierge

---
+++ com.ynov.biblio/src/main/java/com/ynov/biblio/com/ynov/biblio/ExercicesApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.biblio.com.ynov.biblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicesApplication.class, args);
+	}
+
+}
---
+++ com.ynov.biblio/src/test/java/com/ynov/biblio/com/ynov/biblio/ExercicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.biblio.com.ynov.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
